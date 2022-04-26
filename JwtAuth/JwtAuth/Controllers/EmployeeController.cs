7b000d17718d11f29ac82bff4fef451e03e224bb
ibrahim <ibrahimea@code.edu.az>
2022-04-26 07:43:36 +0400

    Create

---
+++ JwtAuth/JwtAuth/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using JwtAuth.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAuth.Controllers
+{
+  
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles ="admin")]
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+
+        [Authorize]
+        [HttpGet]
+        [Route("GetAllByName")]
+        public async Task<IActionResult> GetAllByName([FromQuery] string name)
+        {
+            return Ok(await _context.Employees.Where(m=>m.FullName == name).ToListAsync());
+        }
+
+    }
+}
---
+++ JwtAuth/JwtAuth/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using JwtAuth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuth.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
